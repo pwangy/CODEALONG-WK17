3fd7748c667a496516fe582db454c597e290de09
Peggy <64912576+pwangy@users.noreply.github.com>
2020-12-07 10:34:37 +0100

    progress push

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Technigo Notes - Week 17
 
 Branched for each video/lecture.
+
+Master: Intro to Express - Jennie
+Others are named accordingly.
---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => res.send("Hello, World!"))
+
+app.get("/users", (req, res) => {
+  // normally we would link to a json file. in this example we'll just write some names in an array
+  res.json([
+    { name: "Alice" },
+    { name: "Bob" },
+    { name: "Peggy" },
+    { name: "Chris" },
+    { name: "Jennie" }
+  ])
+})
+
+// the location can be anything we choose. we're using port 3000 here.
+app.listen(3030, () => console.log("App listening to port 3030. Yay!"))
