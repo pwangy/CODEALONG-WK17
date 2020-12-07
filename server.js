56930e21769485a5beb3d86cd85f127cba000d0f
Peggy <64912576+pwangy@users.noreply.github.com>
2020-12-07 11:51:10 +0100

    lecture notes

--- server.js
+++ server.js
@@ -2,18 +2,38 @@ import express from "express"
 
 const app = express()
 
-app.get("/", (req, res) => res.send("Hello, World!"))
+const users = [
+  { id: 1, name: 'Boba', age: 34 },
+  { id: 2, name: 'Ivan', age: 22 },
+  { id: 3, name: 'Van', age: 24 },
+  { id: 4, name: 'Peggy', age: 21 },
+  { id: 5, name: 'Jennie', age: 42 },
+]
 
-app.get("/users", (req, res) => {
-  // normally we would link to a json file. in this example we'll just write some names in an array
-  res.json([
-    { name: "Alice" },
-    { name: "Bob" },
-    { name: "Peggy" },
-    { name: "Chris" },
-    { name: "Jennie" }
-  ])
+
+app.get('/', (request, response) => {
+  response.send('Hello API! Yr documentation better be good!')
 })
 
-// the location can be anything we choose. we're using port 3000 here.
-app.listen(3030, () => console.log("App listening to port 3030. Yay!"))
+app.get('/users', (request, response) => {
+  response.json(users)
+})
+
+app.get('/users/names', (request, response) => {
+  response.json(users.names)
+  console.log(users.names)
+})
+
+
+app.get('/users/:id', (request, response) => {
+  // for example http://localhost:8080/users/1
+  // should print 1 in Terminal
+  // console.log(request.params.id)
+  response.json(request.params)
+  // const { id } = request.params
+})
+
+app.listen(8080, () => {
+  // the server has started, what should it do now?
+  console.log("Hello console, the server is now running.")
+})
